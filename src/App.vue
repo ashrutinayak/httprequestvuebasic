31f2e9c9d490ee5f9fada1827ef1aa1af3f72f6b
weidong <weidongchen.auctionsoftware@gmail.com>
2019-12-11 14:50:08 +0530

    get data demo

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,50 @@
 <template>
   <div>
-    <form v-if="!submitdata">
+    <form v-if="!submitdata && !viewdataes">
       <input type="text" v-model="name" placeholder="Enter person full name" />
       <br />
-      <input
-        type="number"
-        v-model="number"
-        placeholder="Enter person Mobile number"
-      />
+      <input type="number" v-model="number" placeholder="Enter person Mobile number" />
       <br />
       <input type="email" v-model="email" placeholder="Enter person Email" />
       <br />
       <!-- <input type="password" placeholder="Enter the Password" /> -->
       <br />
       <button v-on:click.prevent="handleSubmit()">Submit</button>
+      <button v-on:click.prevent="handleView()">Display</button>
     </form>
     <div v-if="submitdata">
       <h2>Data Submitted Successful.</h2>
+    </div>
+    <div v-if="viewdataes">
+      <h1>List of data View</h1>
+      <div v-if="status">
+        <div v-for="demo in demodata" v-bind:key="demo">
+          <div>{{ demo.title }}</div>
+          <div id="demobody">{{ demo.body}}</div>
+        </div>
+      </div>
+      <div v-if="!status">
+        <h3>No data avaiable</h3>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import viewdata from "./component/viewdata.vue";
 export default {
+  comments: {
+    viewdata: "viewdata"
+  },
   data() {
     return {
       name: "",
       number: "",
       email: "",
-      submitdata: false
+      submitdata: false,
+      viewdataes: false,
+      status: 0,
+      demodata: []
     };
   },
   methods: {
@@ -45,8 +61,25 @@ export default {
         .then(function(data) {
           this.submitdata = true;
         });
+    },
+    handleView: function() {
+      this.$http
+        .post("http://localhost/webserivcedemo/viewstate.php")
+        .then(function(data) {
+          this.viewdataes = true;
+          console.log(data.body);
+          this.demodata = data.body.Demodetail;
+          this.status = data.body.status;
+        });
     }
   }
 };
 </script>
-<style></style>;
+<style>
+h4 {
+  float: left;
+}
+#demobody {
+  /* float: left; */
+}
+</style>;
